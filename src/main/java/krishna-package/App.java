9cea31a15c8a6bce252751a6a5337129ca8c0f4a
krishna-6768 <115453932+krishna-6768@users.noreply.github.com>
2022-10-21 14:02:29 +0530

    Update App.java

--- src/main/java/krishna-package/App.java
+++ src/main/java/krishna-package/App.java
@@ -1,4 +1,10 @@
-public static void main{String[]args}
-{
-System.out.println ("Hello world")
+public class Main {
+  public static void main(String[] args) {
+    int sum1 = 100 + 50;
+    int sum2 = sum1 + 250;
+    int sum3 = sum2 + sum2;
+    System.out.println(sum1);
+    System.out.println(sum2);
+    System.out.println(sum3);  
+  }
 }
